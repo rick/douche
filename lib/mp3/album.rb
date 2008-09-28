c1b6bca82e5180e583f1429699be01d24052be88
Rick Bradley <rick@rickbradley.com>
2008-09-28 02:42:42 -0500

    Lazy load album detail from the album's songs.

--- lib/mp3/album.rb
+++ lib/mp3/album.rb
@@ -3,7 +3,7 @@ require 'mp3/song'
 module MP3
   class Album
     attr_reader :directory
-    attr_accessor :name, :artist, :genre, :multiple_artists
+    attr_writer :name, :artist, :genre, :multiple_artists
     
     def initialize(directory)
       @directory = directory
@@ -15,6 +15,24 @@ module MP3
     
     def song_files
       Dir.open(directory).to_a.select { |f| f =~ /\.mp3$/ }
+    end
+
+    def name
+      @name ||= songs.first.album
+    end
+
+    def artist
+      @artist ||= songs.first.artist
+    end
+
+    def genre
+      @genre ||= songs.first.genre
+    end
+
+    def multiple_artists
+      return @multiple_artists if @multiple_artists
+      song_list = songs
+      @multiple_artists = song_list.any? {|song| song.artist != song_list.first.artist }
     end
   end
 end
--- spec/mp3/album_spec.rb
+++ spec/mp3/album_spec.rb
@@ -43,7 +43,33 @@ describe MP3::Album do
     it 'should allow setting the album name' do
       @album.should respond_to(:name=)
     end
-    
+
+    describe 'reading the album name' do
+      describe 'when the album name is not yet set' do
+        it 'should return the album name from the first song' do
+          @songs = [ @first, 2, 3]
+          stub(@album).songs { @songs }
+          stub(@first).album { 'Hood on the Blighway' }
+          @album.name.should == 'Hood on the Blighway'
+        end
+      end
+      
+      describe 'when the album name has been set' do
+        before :each do
+          @album.name = 'Hood on the Blighway'
+        end
+
+        it 'should not lookup the album name from any songs' do
+          mock(@album).songs.never
+          @album.name
+        end
+        
+        it 'should return the previously set album name' do
+          @album.name.should == 'Hood on the Blighway'
+        end        
+      end
+    end
+
     it 'should allow reading the album artist' do
       @album.should respond_to(:artist)
     end
@@ -51,7 +77,33 @@ describe MP3::Album do
     it 'should allow setting the album artist' do
       @album.should respond_to(:artist=)
     end
-    
+
+    describe 'reading the album artist' do
+      describe 'when the album artist is not yet set' do
+        it 'should return the album artist from the first song' do
+          @songs = [ @first, 2, 3]
+          stub(@album).songs { @songs }
+          stub(@first).artist { 'Bob Dylan' }
+          @album.artist.should == 'Bob Dylan'
+        end
+      end
+      
+      describe 'when the album artist has been set' do
+        before :each do
+          @album.artist = 'Bob Dylan'
+        end
+
+        it 'should not lookup the artist name from any songs' do
+          mock(@album).songs.never
+          @album.artist
+        end
+        
+        it 'should return the previously set artist name' do
+          @album.artist.should == 'Bob Dylan'
+        end
+      end
+    end
+
     it 'should allow reading the album genre' do
       @album.should respond_to(:genre)
     end
@@ -60,12 +112,75 @@ describe MP3::Album do
       @album.should respond_to(:genre=)
     end
     
+    describe 'reading the album genre' do
+      describe 'when the album genre is not yet set' do
+        it 'should return the album genre from the first song' do
+          @songs = [ @first, 2, 3]
+          stub(@album).songs { @songs }
+          stub(@first).genre { 'Blues' }
+          @album.genre.should == 'Blues'
+        end
+      end
+      
+      describe 'when the album genre has been set' do
+        before :each do
+          @album.genre = 'Blues'
+        end
+
+        it 'should not lookup the genre name from any songs' do
+          mock(@album).songs.never
+          @album.genre
+        end
+        
+        it 'should return the previously set genre name' do
+          @album.genre.should == 'Blues'
+        end
+      end
+    end
+
     it 'should allow reading the album multiple artists value' do
       @album.should respond_to(:multiple_artists)
     end
     
     it 'should allow setting the album multiple artists value' do
       @album.should respond_to(:multiple_artists=)
+    end
+
+    describe 'reading the album multiple artists value' do
+      describe 'when the multiple artists value is not yet set' do
+        before :each do
+          @first, @second = { }, { }
+          stub(@first).artist { 'Bob Dylan' }
+          stub(@second).artist { 'Willie Nelson' }
+        end
+        
+        it 'should return true if any songs have differing artists' do
+          @songs = [ @first, @second, @first ]
+          stub(@album).songs { @songs }
+          @album.multiple_artists.should be_true
+        end
+        
+        it 'should return false if all songs have the same artist' do
+          @songs = [ @first, @first, @first ]
+          stub(@album).songs { @songs }
+          @album.multiple_artists.should be_false
+        end
+      end
+      
+      describe 'when the album multiple artists value has been set' do
+        before :each do
+          @album.multiple_artists = :foo
+        end
+
+        it 'should not lookup the genre name from any songs' do
+          mock(@album).songs.never
+          @album.multiple_artists
+        end
+        
+        it 'should return the previously set genre name' do
+          @album.multiple_artists.should == :foo
+        end
+      end
     end
 
     describe 'when looking up the songs in the album' do
