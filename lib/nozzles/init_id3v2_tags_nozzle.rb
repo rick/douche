e28770eb57a830c069279fd9166cf0ad1345a933
Rick Bradley <rick@rickbradley.com>
2008-10-01 18:23:58 -0500

    Streamlining id3v2 tag initializing nozzle; using mp3info more robustly.

--- lib/nozzles/init_id3v2_tags_nozzle.rb
+++ lib/nozzles/init_id3v2_tags_nozzle.rb
@@ -4,48 +4,33 @@ require 'nozzle'
 
 class InitId3v2TagsNozzle < Nozzle
   def stank?(file)
-    if params['pattern']
-      return false if params['pattern'] and !(file =~ Regexp.new(params['pattern']))
-    end
+    return false if params['pattern'] and !(file =~ Regexp.new(params['pattern']))
     ! douched?(file)
   end
 
   def spray(file)
-    need = needs_v2_tags?(file)
-    upgrade_tags(file) if need == :upgrade
-    create_tags(file) if need == :create
-    douched(file)
-    return true
+    upgrade_tags(file) if needs_v2_tags?(file)
   rescue Exception => e
     puts "Warning: could not read id3 tags in file [#{file}] [#{e.to_s}]"
     flag_error(file)
+  ensure
     douched(file)
     return true
   end
 
   def needs_v2_tags?(file)
     info = Mp3Info.open(file)
-    return false if info.hastag2?
-    info.hastag1? ? :upgrade : :create
+    ! info.hastag2?
   end
 
   def upgrade_tags(file)
-    puts "Upgrading tags from v1 to v2 for file [#{file}]"
+    puts "Upgrading tags to id3v2 for file [#{file}]"
     Mp3Info.open(file) do |mp3|
-      mp3.tag2.title = mp3.tag1.title
-      mp3.tag2.artist = mp3.tag1.artist
-      mp3.tag2.album = mp3.tag1.album
-      mp3.tag2.genre = mp3.tag1.genre
-    end
-  end
-
-  def create_tags(file)
-    puts "Creating empty v2 tags for file [#{file}]"
-    Mp3Info.open(file) do |mp3|
-      mp3.tag2.title = ''
-      mp3.tag2.artist = ''
-      mp3.tag2.album = ''
-      mp3.tag2.genre = ''
+      mp3.tag2.TCON = '(%02d)' % (mp3.tag1['genre'] ||'').to_s.gsub(/[()]/, '').to_i
+      mp3.tag2.TIT2 = (mp3.tag1['title']  || '').strip
+      mp3.tag2.TPE1 = (mp3.tag1['artist'] || '').strip
+      mp3.tag2.TALB = (mp3.tag1['album']  || '').strip
+      mp3.flush
     end
   end
   
--- spec/nozzles/init_id3v2_tags_nozzle_spec.rb
+++ spec/nozzles/init_id3v2_tags_nozzle_spec.rb
@@ -9,21 +9,21 @@ describe InitId3v2TagsNozzle do
   before :each do
     @config = DoucheConfig.new(:directory => '/path/to/something')
   end
-  
+
   describe 'when initializing' do
     it 'should accept a config object' do
       lambda { InitId3v2TagsNozzle.new(:foo) }.should_not raise_error(ArgumentError)
     end
-    
+
     it 'should require a config object' do
       lambda { InitId3v2TagsNozzle.new }.should raise_error(ArgumentError)
     end
-    
+
     it 'should return a Nozzle' do
       InitId3v2TagsNozzle.new(@config).is_a?(Nozzle).should be_true
     end
   end
-  
+
   describe 'once initialized' do
     before :each do
       @options = { :directory => '/path/to' }
@@ -46,37 +46,31 @@ describe InitId3v2TagsNozzle do
         stub(@info).hastag2? { false }
         stub(Mp3Info).open(@file) { @info }
       end
-      
+
       it 'should accept a file path' do
         lambda { @nozzle.needs_v2_tags?(@file) }.should_not raise_error(ArgumentError)
       end
-      
+
       it 'should require a file path' do
         lambda { @nozzle.needs_v2_tags? }.should raise_error(ArgumentError)
       end
-      
+
       it 'should read the id3 tag info for the file' do
         mock(Mp3Info).open(@file) { @info }
         @nozzle.needs_v2_tags?(@file)
       end
-      
-      it 'should return :create when there are no id3 tags for the file' do
+
+      it 'should return true when there are no id3v2 tags for the file' do
         stub(@info).hastag1? { false }
         stub(@info).hastag2? { false }
-        @nozzle.needs_v2_tags?(@file).should == :create
+        @nozzle.needs_v2_tags?(@file).should be_true
       end
-      
-      it 'should return :upgrade when there are only id3v1 tags for the file' do
-        stub(@info).hastag1? { true }
-        stub(@info).hastag2? { false }
-        @nozzle.needs_v2_tags?(@file).should == :upgrade
-      end
-      
+
       it 'should return false when there are id3v2 tags for the file' do
         stub(@info).hastag2? { true }
         @nozzle.needs_v2_tags?(@file).should be_false
       end
-        
+
       it 'should raise an Mp3InfoError when the id3 tags are unreadable' do
         mock(Mp3Info).open(@file) { raise Mp3InfoError }
         lambda { @nozzle.needs_v2_tags?(@file) }.should raise_error(Mp3InfoError)
@@ -91,102 +85,84 @@ describe InitId3v2TagsNozzle do
       it 'should accept a file path' do
         lambda { @nozzle.upgrade_tags(@file) }.should_not raise_error(ArgumentError)
       end
-      
+
       it 'should require a file path' do
         lambda { @nozzle.upgrade_tags }.should raise_error(ArgumentError)
       end
-      
+
       it 'should set id3v2 tags for artist, album, genre, and title from the id3v1 tags' do
         pending("figuring out how to test this")
         @nozzle.upgrade_tags(@file)
-      end        
+      end
     end
 
-    it 'should have a means of creating id3 tags' do
-      @nozzle.should respond_to(:create_tags)
-    end
-    
-    describe 'when creating id3 tags' do
-      it 'should accept a file path' do
-        lambda { @nozzle.create_tags(@file) }.should_not raise_error(ArgumentError)
-      end
-      
-      it 'should require a file path' do
-        lambda { @nozzle.create_tags }.should raise_error(ArgumentError)
-      end
-      
-      it 'should set empty id3v2 tags for artist, album, genre, and title' do
-        pending("figuring out how to test this")
-      end
-    end
-    
     it 'should have a means of marking a file as having an encoding error' do
       @nozzle.should respond_to(:flag_error)
-    end    
+    end
 
     describe 'when marking a file as having an encoding error' do
       before :each do
         @badfile = File.join(File.dirname(@file), ".douche_error_encoding-#{File.basename(@file, '.mp3')}")
         stub(File).open(@badfile, 'w')
       end
-      
+
       it 'should accept a file path' do
         lambda { @nozzle.flag_error(@file) }.should_not raise_error(ArgumentError)
       end
-      
+
       it 'should require a file path' do
         lambda { @nozzle.flag_error }.should raise_error(ArgumentError)
       end
-      
+
       it 'should touch a bad encoding file for the provided file' do
         mock(File).open(@badfile, 'w')
         @nozzle.flag_error(@file)
       end
     end
-    
+
     describe 'when checking if a file is stank' do
       it 'should accept a filename' do
         lambda { @nozzle.stank?(:foo) }.should_not raise_error(ArgumentError)
       end
-      
+
       it 'should require a filename' do
-        lambda { @nozzle.stank? }.should raise_error(ArgumentError)        
+        lambda { @nozzle.stank? }.should raise_error(ArgumentError)
       end
 
       before :each do
         stub(@nozzle).params { { } }
       end
-      
+
       describe 'when there is a pattern specified in params and the file does not match' do
-        before :each do          
+        before :each do
           stub(@nozzle).params { { 'pattern' => '\.mp3$' } }
         end
-        
+
         it 'should return false' do
           @nozzle.stank?('/path/to/some.mp4').should be_false
         end
       end
-      
+
       it "should check if the file has already been processed before" do
         mock(@nozzle).douched?(@file)
         @nozzle.stank?(@file)
       end
-      
+
       describe 'if the file has already been processed before' do
         before :each do
           stub(@nozzle).douched?(@file) { true }
         end
-        
+
         it 'should return false' do
           @nozzle.stank?(@file).should be_false
         end
       end
-      
+
       describe 'if the file has not been processed before' do
         before :each do
           stub(@nozzle).douched?(@file) { false }
         end
-        
+
         it 'should return true' do
           @nozzle.stank?(@file).should be_true
         end
@@ -202,7 +178,7 @@ describe InitId3v2TagsNozzle do
         stub(@nozzle).needs_v2_tags?(@file) { false }
         stub(@nozzle).flag_error(@file)
       end
-      
+
       it 'should accept a filename' do
         lambda { @nozzle.spray(@file) }.should_not raise_error(ArgumentError)
       end
@@ -220,19 +196,14 @@ describe InitId3v2TagsNozzle do
         before :each do
           stub(@nozzle).needs_v2_tags?(@file) { raise Mp3InfoError }
         end
-        
+
         it 'should output a warning' do
           mock(@nozzle).puts(anything)
           @nozzle.spray(@file)
         end
-        
+
         it 'should create a bad-encoding marker file' do
           mock(@nozzle).flag_error(@file)
-          @nozzle.spray(@file)
-        end
-        
-        it 'should not create id3v2 tags' do
-          mock(@nozzle).create_tags(@file).never
           @nozzle.spray(@file)
         end
 
@@ -240,12 +211,12 @@ describe InitId3v2TagsNozzle do
           mock(@nozzle).upgrade_tags(@file).never
           @nozzle.spray(@file)
         end
-        
+
         it 'should mark the file as douched' do
           mock(@nozzle).douched(@file)
           @nozzle.spray(@file)
         end
-        
+
         it 'should return true' do
           @nozzle.spray(@file).should be_true
         end
@@ -255,17 +226,12 @@ describe InitId3v2TagsNozzle do
         before :each do
           stub(@nozzle).needs_v2_tags?(@file) { false }
         end
-        
+
         it 'should not create a bad-encoding marker file' do
           mock(@nozzle).flag_error(@file).never
           @nozzle.spray(@file)
         end
-        
-        it 'should not create id3v2 tags' do
-          mock(@nozzle).create_tags(@file).never
-          @nozzle.spray(@file)
-        end
-          
+
         it 'should not upgrade id3v2 tags' do
           mock(@nozzle).upgrade_tags(@file).never
           @nozzle.spray(@file)
@@ -275,71 +241,34 @@ describe InitId3v2TagsNozzle do
           mock(@nozzle).douched(@file)
           @nozzle.spray(@file)
         end
-        
+
         it 'should return true' do
           @nozzle.spray(@file).should be_true
         end
       end
-      
+
       describe 'when the file has no id3v2 tags' do
-        describe 'but it has id3v1 tags' do
-          before :each do
-            stub(@nozzle).needs_v2_tags?(@file) { :upgrade }
-          end
-          
-          it 'should not create a bad-encoding marker file' do
-            mock(@nozzle).flag_error(@file).never
-            @nozzle.spray(@file)
-          end
-          
-          it 'should not create id3v2 tags' do
-            mock(@nozzle).create_tags(@file).never
-            @nozzle.spray(@file)
-          end
-          
-          it 'should upgrade id3v2 tags' do
-            mock(@nozzle).upgrade_tags(@file)
-            @nozzle.spray(@file)
-          end
-          
-          it 'should mark the file as douched' do
-            mock(@nozzle).douched(@file)
-            @nozzle.spray(@file)
-          end
-          
-          it 'should return true' do
-            @nozzle.spray(@file).should be_true
-          end
+        before :each do
+          stub(@nozzle).needs_v2_tags?(@file) { true }
         end
 
-        describe 'and it has no id3v1 tags' do
-          before :each do
-            stub(@nozzle).needs_v2_tags?(@file) { :create }
-          end
-          
-          it 'should not create a bad-encoding marker file' do
-            mock(@nozzle).flag_error(@file).never
-            @nozzle.spray(@file)
-          end
-          
-          it 'should not upgrade id3v2 tags' do
-            mock(@nozzle).upgrade_tags(@file).never
-            @nozzle.spray(@file)
-          end
-          
-          it 'should create id3v2 tags' do
-            mock(@nozzle).create_tags(@file)
-            @nozzle.spray(@file)
-          end
-          
-          it 'should mark the file as douched' do
-            mock(@nozzle).douched(@file)
-            @nozzle.spray(@file)
-          end
-          
-          it 'should return true' do
-            @nozzle.spray(@file).should be_true
-          end
+        it 'should not create a bad-encoding marker file' do
+          mock(@nozzle).flag_error(@file).never
+          @nozzle.spray(@file)
+        end
+
+        it 'should upgrade id3v2 tags' do
+          mock(@nozzle).upgrade_tags(@file)
+          @nozzle.spray(@file)
+        end
+
+        it 'should mark the file as douched' do
+          mock(@nozzle).douched(@file)
+          @nozzle.spray(@file)
+        end
+
+        it 'should return true' do
+          @nozzle.spray(@file).should be_true
         end
       end
     end
